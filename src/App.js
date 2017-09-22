7c503c192f1b519556fd610ad7d3a6807c9fed83
Nathan Wells <nwwells@gmail.com>
2017-09-22 19:05:42 -0400

    Add tools (sourcemap analyzer, precommit lint)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import { Button } from 'semantic-ui-react';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">Node Admin</div>
+      <div className="App">
+        Node Admin
+        <Button>Click Here</Button>
+      </div>
     );
   }
 }
