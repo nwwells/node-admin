36ceb1315fc813f00d775f35e6543f8b256edf7d
Nathan Wells <nwwells@gmail.com>
2017-09-26 19:09:43 -0400

    Beginnings....

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
-import { Button } from 'semantic-ui-react';
+import { Breadcrumb, Header } from 'semantic-ui-react';
+
 import './App.css';
 
 const App = () => (
   <div className="App">
-    Node Admin
-    <Button>Click Here</Button>
+    <div className="App-header">
+      <Breadcrumb size="huge">
+        {/* <Breadcrumb.Section link>Home</Breadcrumb.Section>
+        <Breadcrumb.Divider icon="right chevron" />
+        <Breadcrumb.Section link>Registration</Breadcrumb.Section>
+        <Breadcrumb.Divider icon="right chevron" /> */}
+        <Breadcrumb.Section active>System Status</Breadcrumb.Section>
+      </Breadcrumb>
+    </div>
+    <div className="App-main">
+      <div className="App-page">
+        <Header as="h1">System Events - Last 24 Hours</Header>
+      </div>
+    </div>
   </div>
 );
 
